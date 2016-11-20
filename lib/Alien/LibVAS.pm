55cd0bd8bda259b321cc2d9bf4a59fa74d4e7ef1
Ahmad Fatoum <ahmad@a3f.at>
2016-11-20 14:41:17 +0100

    Initial commit

---
+++ lib/Alien/LibVAS.pm
@@ -0,0 +1,52 @@
+use strict;
+use warnings;
+package Alien::LibVAS;
+
+# ABSTRACT: Perl distribution for libvas
+# VERSION
+
+use parent 'Alien::Base';
+1;
+__END__
+
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Alien::LibVAS - Perl distribution for libvas
+
+=head1 SYNOPSIS
+
+    cpan Alien::LibVAS
+
+
+=head1 DESCRIPTION
+
+See L<Proc::Memory> for a Perl wrapper that makes use of this distribution.
+
+=cut
+
+=head1 GIT REPOSITORY
+
+L<http://github.com/athreef/Alien-LibVAS>
+
+=head1 SEE ALSO
+
+L<Proc::Memory>
+L<Alien>
+
+=head1 AUTHOR
+
+Ahmad Fatoum C<< <athreef@cpan.org> >>, L<http://a3f.at>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2016 Ahmad Fatoum
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/init.t
@@ -0,0 +1,11 @@
+use Test::More;
+
+BEGIN {
+    use_ok '';
+}
+
+my $instance = ->new();
+
+done_testing;
+
+
